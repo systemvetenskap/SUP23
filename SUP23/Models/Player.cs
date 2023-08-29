53178bf035873235e252f815096ba66b84d6c605
Erik Öberg <erik.oberg@miun.se>
2023-08-29 15:05:12 +0200

    Pratar om arv, abstract, interface.
    implementerar INotifyPropertyChanged
    
    Lägger till Fody

---
+++ SUP23/Models/Player.cs
@@ -0,0 +1,56 @@
+﻿using SUP23.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUP23.Models
+{
+    public abstract class Player : BaseViewModel, IHaveId
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Return current player score
+        /// </summary>
+        /// <returns>curren score</returns>
+        public virtual int GetScore() 
+        { 
+            return 0; 
+        }
+
+        public abstract void SetPlayerName(string name);
+    }
+
+    public class Human : Player
+    {
+        public override int GetScore()
+        {
+            return base.GetScore();
+        }
+
+        public override void SetPlayerName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Computer : Player
+    {
+        public override void SetPlayerName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Card : IHaveId
+    {
+        public int Id { get; set; }
+    }
+
+    public interface IHaveId
+    {
+        int Id { get; set; }
+    }
+}
--- SUP23/ViewModels/MainViewModel.cs
+++ SUP23/ViewModels/MainViewModel.cs
@@ -1,15 +1,27 @@
-﻿using System;
+﻿using SUP23.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SUP23.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : BaseViewModel
     {
         // viktigt.
         // Det måste vara properties 
-        public string Firstname { get; set; } = "Erik";
+        public string FirstName { get; set; }
+        public MainViewModel()
+        {
+            IHaveId haveId = new Computer();
+            IEnumerable<IHaveId> objectsWithIds = new List<IHaveId>();
+            Player player = new Human();
+            player.Name = FirstName;
+            player.GetScore();
+        }
+     
     }
 }
