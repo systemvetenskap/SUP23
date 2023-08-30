edc0735599564a1bc2974db4fbb44943bda25fd3
Erik Öberg <erik.oberg@miun.se>
2023-08-30 14:57:39 +0200

    La till basviewmodel

--- SUP23/Models/Wizard.cs
+++ SUP23/Models/Wizard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SUP23.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,7 @@ namespace SUP23.Models
     /// <summary>
     /// Wizard with magic skills
     /// </summary>
-    public class Wizard
+    public class Wizard : BaseViewModel
     {
         public Wizard(string firstname, string lastname)
         {
